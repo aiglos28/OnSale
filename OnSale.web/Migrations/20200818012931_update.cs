5c2090c33ab9db1352476e5ecad419bb5ee0e4be
YUBAN DAVID OSORNO VILLA <davidosornovilla@gmail.com>
2020-08-18 20:13:45 -0500

    create a new branch

---
+++ OnSale.web/Migrations/20200818012931_update.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnSale.web.Migrations
+{
+    public partial class update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
